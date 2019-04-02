30a423aa382a1d31370814b2bd04f9a6444e5f1c
willParkerBoi <tama3million@gmail.com>
2019-04-02 13:00:58 +0100

    Initial game files push

---
+++ scene/Atmosphere.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Atmosphere : MeshInstance
+{
+    // Declare member variables here.
+    private float speed = 1;
+    private Node object = GetNode("Atmosphere");
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+        while(true){
+        
+        }
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
